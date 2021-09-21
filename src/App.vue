1d0b972cb099ebeeb1d1a2897144ccb9462b9f27
Elyse <kevincristian@outlook.com>
2021-09-21 14:32:58 -0500

    Iframe post message working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <input v-model="equation">
+    {{ derivative }}
+
+    Latex: {{latex}}
+
+    <vue-iframe
+        src="/plot/plot.html"
+        frame-id="my-iframe"
+        name="my-frame"
+        @load="onLoadIframe"
+        width="700px"
+        height="500px"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {create, all} from 'mathjs'
+
+const config = {}
+const math = create(all, config)
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data: function () {
+    return {
+      equation: '',
+      myIframe: null
+    }
+  },
+  methods: {
+    onLoadIframe(frame) {
+      console.log("Iframe loaded")
+      this.myIframe = frame.contentWindow
+    }
+  },
+  computed: {
+    derivative() {
+      try {
+        return math.derivative(this.equation, 'x')
+      } catch (e) {
+        console.log("Error getting derivate")
+        return math.parse('0')
+      }
+    },
+    latex() {
+      return this.derivative.toTex()
+    }
+  },
+  watch: {
+    latex: function (value){
+      this.myIframe.postMessage(value.toString())
+    },
+    derivative: function () {
+    },
+    equation: function () {
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import VueIframe from 'vue-iframes'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueIframe)
 
 new Vue({
   render: h => h(App),
