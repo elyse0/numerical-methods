cdea45f125c4fb425a5786a73bdacca870a5a144
Elyse <kevincristian@outlook.com>
2021-09-25 20:48:57 -0500

    Bisection: Add AppBisectionIterationButtons.vue
    This component contains the three points of a
    Bisection iteration, using the
    AppBisectionPointButton component.

---
+++ src/components/Bisection/AppBisectionIterationButtons.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="columns is-mobile is-centered">
+      <div class="column has-text-centered">
+        <h5 class="subtitle is-5">P1</h5>
+        <app-bisection-point-button :point="iteration.p1" :precision="precision"/>
+      </div>
+      <div class="column has-text-centered">
+        <h5 class="subtitle is-5">P3</h5>
+        <app-bisection-point-button :point="iteration.p3" :precision="precision"/>
+      </div>
+      <div class="column has-text-centered">
+        <h5 class="subtitle is-5">P2</h5>
+        <app-bisection-point-button :point="iteration.p2" :precision="precision"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import AppBisectionPointButton from '@/components/Bisection/AppBisectionPointButton.vue'
+import {BisectionIteration} from '@/methods/Bisection'
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: "AppBisectionIterationButtons",
+  components: {AppBisectionPointButton},
+  props: {
+    iteration: {type: Object as () => BisectionIteration},
+    precision: {type: Number}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
