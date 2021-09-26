14a215d3067f97a534a0ce9ecb52f5a90655cd8d
Elyse <kevincristian@outlook.com>
2021-09-25 20:46:37 -0500

    Bisection: Add AppBisectionPointButton.vue
    This component contains a button with a tooltip that
    shows x value, fx value and the sign as its color.
    
    Blue - Negative
    Red - Positive
    Green - Zero

---
+++ src/components/Bisection/AppBisectionPointButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-tooltip position="is-bottom" type="is-light">
+      <b-button :label="label" :type="buttonType" outlined expanded/>
+      <template v-slot:content>
+        <p><b>fx: </b> {{point.fx}}</p>
+      </template>
+    </b-tooltip>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {BisectionIterationPoint} from '@/methods/Bisection'
+
+export default Vue.extend({
+  name: "AppBisectionPointButton",
+  props: {
+    point: {type: Object as () => BisectionIterationPoint},
+    precision: {type: Number}
+  },
+  computed: {
+    label(): String {
+      return "x: " + parseFloat(this.point.x.toString()).toFixed(this.precision)
+    },
+    buttonType(): String {
+      if (!this.point)
+        return ""
+
+      if (this.point.sign === 1)
+        return "is-danger"
+
+      if (this.point.sign === -1)
+        return "is-info"
+
+      return "is-success"
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
