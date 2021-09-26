6e573b65a1aedb6cd9d77756d88fa31a84396a0f
Elyse <kevincristian@outlook.com>
2021-09-25 20:28:30 -0500

    Add Katex to use Latex expressions in frontend

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import Buefy from 'buefy'
 // @ts-ignore
 import VueIframe from 'vue-iframes'
+// @ts-ignore
+import VueKatex from 'vue-katex';
 
 // Stylesheet
 import 'buefy/dist/buefy.css'
+import 'katex/dist/katex.min.css';
 
 Vue.use(Buefy)
 Vue.use(VueIframe)
+Vue.use(VueKatex)
 
 Vue.config.productionTip = false
 
