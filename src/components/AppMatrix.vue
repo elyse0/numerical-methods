994c72223efcbb8b21ecb85046706bd9b8e79c5c
Elyse <kevincristian@outlook.com>
2021-11-13 20:36:34 -0600

    Router: Add AppMatrix component
    This component should allow us to create a matrix of input values

---
+++ src/components/AppMatrix.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Matrix!!!
+    <b-numberinput :controls="false"></b-numberinput>
+  </div>
+</template>
+
+<script lang="ts">
+
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class AppMatrix extends Vue {
+
+  columns: number =  2
+  rows: number =  2
+}
+</script>
+
+<style scoped>
+
+</style>
