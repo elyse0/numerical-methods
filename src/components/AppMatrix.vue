76a25b0caa80fe4e2e8b0d52074b773f798e4397
Elyse <kevincristian@outlook.com>
2021-11-15 22:22:09 -0600

    AppMatrix: Add first version of Matrix component

--- src/components/AppMatrix.vue
+++ src/components/AppMatrix.vue
@@ -1,19 +1,44 @@
 <template>
   <div>
     Matrix!!!
-    <b-numberinput :controls="false"></b-numberinput>
+
+    <div class="columns">
+      <div class="column has-text-centered" v-for="column in columns" :key="column">
+        <div class="row" v-for="row in rows" :key="row" style="padding-bottom: 10px">
+          <b-numberinput :controls="false" v-model="matrix[row - 1][column - 1]">
+          </b-numberinput>
+        </div>
+      </div>
+    </div>
+
+
+    Valid: {{valid}}
   </div>
 </template>
 
 <script lang="ts">
-
-import {Component, Vue} from 'vue-property-decorator'
+import {Component, Prop, VModel, Vue, Watch} from 'vue-property-decorator'
 
 @Component
 export default class AppMatrix extends Vue {
 
-  columns: number =  2
-  rows: number =  2
+  @Prop({default: 3}) readonly columns!: number
+  @Prop({default: 3}) readonly rows!: number
+
+  @VModel() matrix: number[][] = Array(this.rows).fill(0).map(() => new Array(this.columns).fill(0))
+
+  get valid(): boolean {
+    const flat = this.matrix.flat()
+
+    const valid = flat.some((value: number | null) => value === null)
+
+    return !valid
+  }
+
+  @Watch("matrix")
+  onMatrix() {
+    console.log(this.matrix)
+  }
 }
 </script>
 
