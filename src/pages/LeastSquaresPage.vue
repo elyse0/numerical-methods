76788302fbf443d55fa2217c1bdd8ac6522d88cd
Elyse <kevincristian@outlook.com>
2021-11-15 22:23:28 -0600

    LeastSquares: Add plot view to page

--- src/pages/LeastSquaresPage.vue
+++ src/pages/LeastSquaresPage.vue
@@ -1,24 +1,41 @@
 <template>
   <AppLayout>
-
-    <b-field label="Rounded">
-      <b-input placeholder="No label"  rounded></b-input>
-    </b-field>
+    <br>
+    <div class="columns">
+      <div class="column">
+        <AppPointsList></AppPointsList>
+      </div>
+      <div class="column">
+        <vue-iframe :src="plotUrl" @load="onLoadPlot"/>
+      </div>
+    </div>
 
   </AppLayout>
 </template>
 
 <script lang="ts">
+import AppPointsList from '@/components/AppPointsList.vue'
 import {Component, Vue} from 'vue-property-decorator'
 
 import AppLayout from '@/components/layout/AppLayout.vue'
 
 @Component({
-  components: {AppLayout}
+  components: {AppPointsList, AppLayout}
 })
 
 export default class LeastSquaresPage extends Vue {
 
+  selectedIteration: number = 1
+  plot: Window | null = null
+
+  plotUrl = `${process.env.BASE_URL}/plot/fixed-point.html`
+
+  onLoadPlot(frame: HTMLFrameElement) {
+    const window = frame.contentWindow
+
+    if (window)
+      this.plot = window
+  }
 }
 </script>
 
