c9abcc03cd4cd51b25d663d10b44c8dfffe5197f
Elyse <kevincristian@outlook.com>
2021-11-18 00:09:47 -0600

    AppHero: Add AppHero component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <p class="title" v-if="title">
+        {{ title }}
+      </p>
+      <p class="subtitle" v-if="subtitle">
+        {{subtitle}}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator'
+
+@Component
+export default class AppHero extends Vue {
+
+  @Prop({default: null}) readonly title!: string | null
+  @Prop({default: null}) readonly subtitle!: string | null
+}
+</script>
+
+<style scoped>
+
+</style>
