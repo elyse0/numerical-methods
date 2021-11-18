afc43d177a6293bd2c45ea83747e771792484300
Elyse <kevincristian@outlook.com>
2021-11-17 21:18:33 -0600

    AppContentLayout: Add ContentLayout component

---
+++ src/components/layout/AppContentLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <AppLayout>
+    <div class="content-layout">
+      <main class="layout_main">
+        <slot/>
+      </main>
+    </div>
+  </AppLayout>
+</template>
+
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+import AppLayout from '@/components/layout/AppLayout.vue'
+
+@Component({
+  components: {AppLayout}
+})
+
+export default class AppContentLayout extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
