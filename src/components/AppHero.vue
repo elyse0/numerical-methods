2420f8b9f752057dc2b3e683ed63386b73c1677a
Elyse <kevincristian@outlook.com>
2021-11-19 21:14:41 -0600

    AppHero: Add padding variable

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero">
+  <section :class="classType" :style="cssVars">
     <div class="hero-body">
       <p class="title" v-if="title">
         {{ title }}
@@ -19,9 +19,24 @@ export default class AppHero extends Vue {
 
   @Prop({default: null}) readonly title!: string | null
   @Prop({default: null}) readonly subtitle!: string | null
+  @Prop({default: false}) readonly centered!: boolean
+  @Prop({default: 3}) readonly padding!: number
+
+  get classType(): string{
+    return this.centered ? "hero has-text-centered" : "hero"
+  }
+
+  get cssVars() {
+    return {
+      "--padding": `${this.padding}rem ${this.padding}rem`
+    }
+  }
+
 }
 </script>
 
 <style scoped>
-
+.hero-body {
+  padding: var(--padding);
+}
 </style>
