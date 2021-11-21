e0f7955cf7e227d2afaa3c4654b865bb9d0de2f0
Elyse <kevincristian@outlook.com>
2021-11-20 20:55:52 -0600

    TrapezoidalIntegration: Improve readability

--- src/methods/TrapezoidalIntegration.ts
+++ src/methods/TrapezoidalIntegration.ts
@@ -1,25 +1,25 @@
 import NumericalMethod, {IntegrationInterval} from '@/methods/NumericalMethod'
-import {MathNode} from 'mathjs'
+import {EvalFunction, MathNode} from 'mathjs'
 
 class TrapezoidalIntegration extends NumericalMethod {
 
 
-    static method(inputFunctionFx: MathNode, interval: IntegrationInterval, steps: number): number | null {
-        const subintervalWidth = (interval.x0 + interval.x1) / steps
+    static method(functionFx: EvalFunction, interval: IntegrationInterval, steps: number): number | null {
+        const a = interval.x0
+        const b = interval.x0
+        const subintervalWidth = (a + b) / steps
 
         const xValues: number[] = []
-        for (let i = interval.x0; i <= interval.x1; i = i + subintervalWidth) {
+        for (let i = a + subintervalWidth; i <= b - subintervalWidth; i = i + subintervalWidth) {
             xValues.push(i)
         }
+        const fxiValues: number[] = xValues.map((x) => this.evaluate(functionFx, x))
 
-        const fxValues: number[] = xValues.map((value) => {
-            return TrapezoidalIntegration.evaluate(inputFunctionFx.compile(), value)
-        })
+        const fxa = this.evaluate(functionFx, a)
+        const fxb = this.evaluate(functionFx, a)
+        const sumFxi = 2 * this.sum(fxiValues)
 
-        const sumFxi = 2 * fxValues.slice(1, fxValues.length - 1).reduce((a, b) => a + b, 0)
-        const completeSum = fxValues[0] + fxValues[fxValues.length - 1] + sumFxi
-
-        return (subintervalWidth / 2) * completeSum
+        return (subintervalWidth / 2) * (fxa + sumFxi + fxb)
     }
 }
 
