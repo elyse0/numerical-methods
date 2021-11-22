04654b2fc9ec6522893e29b20a40c1dbc20f708d
Elyse <kevincristian@outlook.com>
2021-11-21 21:18:21 -0600

    SimpsonIntegration: Add create method

--- src/methods/SimpsonIntegration.ts
+++ src/methods/SimpsonIntegration.ts
@@ -8,7 +8,56 @@ enum SimpsonIntegrationType {
 
 class SimpsonIntegration extends NumericalMethod {
 
-    public static OneThirdMethod(functionFx: EvalFunction, interval: IntegrationInterval, steps: number) {
+    type: SimpsonIntegrationType
+    integral: number
+    functionFx: string
+    integralFunction: string
+    integrationInterval: string
+
+    private constructor(
+        type: SimpsonIntegrationType,
+        integral: number,
+        functionFx: string,
+        integralFunction: string,
+        integrationInterval: string){
+        super()
+        this.type = type
+        this.integral = integral
+        this.functionFx = functionFx
+        this.integralFunction = integralFunction
+        this.integrationInterval = integrationInterval
+    }
+
+    public static create(
+        functionFx: string,
+        interval: IntegrationInterval,
+        steps: number): SimpsonIntegration | null {
+        const parsedFunction = this.getParsedFunction(functionFx)
+        if (!parsedFunction) {
+            console.log("SimpsonIntegration: Error parsing function")
+            return null
+        }
+        // FIXME: Verify interval and steps
+        let integral: number
+        let type: SimpsonIntegrationType
+        if (steps % 2 === 0) {
+            type = SimpsonIntegrationType.OneThird
+            integral = this.OneThirdMethod(parsedFunction, interval, steps)
+        } else {
+            type = SimpsonIntegrationType.ThreeEighth
+            integral = this.ThreeEighthMethod(parsedFunction, interval, steps)
+        }
+        if (!integral) {
+            console.log("TrapezoidalIntegration: Error computing method")
+            return null
+        }
+        const functionFxTex = this.getFxEquation(parsedFunction.toString())
+        const integralFunction = this.getFxIntegral(functionFx, interval)
+        const integrationInterval = this.getIntegrationIntervalString(interval)
+        return new SimpsonIntegration(type, integral, functionFxTex, integralFunction, integrationInterval)
+    }
+
+    public static OneThirdMethod(functionFx: EvalFunction, interval: IntegrationInterval, steps: number): number {
         const a = interval.x0
         const b = interval.x1
         const subintervalWidth = (b - a) / steps
@@ -20,8 +69,8 @@ class SimpsonIntegration extends NumericalMethod {
         const fxiValues: number[] = xValues.map((x) => this.evaluate(functionFx, x))
 
         // Note: They're inverted because we started at 1
-        const oddFxi = fxiValues.filter(((value, index) => index % 2 === 0))
         const evenFxi = fxiValues.filter(((value, index) => index % 2 === 1))
+        const oddFxi = fxiValues.filter(((value, index) => index % 2 === 0))
 
         const fxa = this.evaluate(functionFx, a)
         const fxb = this.evaluate(functionFx, b)
@@ -32,8 +81,17 @@ class SimpsonIntegration extends NumericalMethod {
     }
 
     public static ThreeEighthMethod(functionFx: EvalFunction, interval: IntegrationInterval, steps: number) {
+        // FIXME: Verify that steps is 3
+        const a = interval.x0
+        const b = interval.x1
+        const subIntervalWidth = (b - a) / 3
 
+        const fx0= this.evaluate(functionFx, a)
+        const fx1= this.evaluate(functionFx, a + subIntervalWidth)
+        const fx2= this.evaluate(functionFx, a + (subIntervalWidth * 2))
+        const fx3= this.evaluate(functionFx, b)
 
+        return (subIntervalWidth * (3 / 8)) * (fx0 + (3 * fx1) + (3 * fx2) + fx3)
     }
 }
 
