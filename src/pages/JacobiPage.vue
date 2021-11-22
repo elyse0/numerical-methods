0bf1fd75aee910e0fae457917c7b4ecd0a54d93d
Elyse <kevincristian@outlook.com>
2021-11-21 23:52:43 -0600

    Jacobi: Use AppPlot component

--- src/pages/JacobiPage.vue
+++ src/pages/JacobiPage.vue
@@ -6,7 +6,7 @@
         <AppMatrix></AppMatrix>
       </div>
       <div class="column">
-        <vue-iframe :src="plotUrl" @load="onLoadPlot"/>
+        <AppPlot name="jacobi" v-model="plot"/>
       </div>
     </div>
 
@@ -17,10 +17,11 @@
 import {Component, Vue} from 'vue-property-decorator'
 
 import AppContentLayout from '@/components/layout/AppContentLayout.vue'
+import AppPlot from '@/components/AppPlot.vue'
 import AppMatrix from '@/components/AppMatrix.vue'
 
 @Component({
-  components: {AppMatrix, AppContentLayout}
+  components: {AppContentLayout, AppPlot, AppMatrix}
 })
 
 export default class JacobiPage extends Vue {
@@ -28,14 +29,6 @@ export default class JacobiPage extends Vue {
   selectedIteration: number = 1
   plot: Window | null = null
 
-  plotUrl = `${process.env.BASE_URL}/plot/jacobi.html`
-
-  onLoadPlot(frame: HTMLFrameElement) {
-    const window = frame.contentWindow
-
-    if (window)
-      this.plot = window
-  }
 }
 </script>
 
