9c6ba0da52f43103d0548e10d06fc8a98dbf5701
Elyse <kevincristian@outlook.com>
2021-11-21 23:20:17 -0600

    TaylorSeries: Add TaylorSeries page

---
+++ src/methods/TaylorSeries.ts
@@ -0,0 +1,49 @@
+import NumericalMethod from '@/methods/NumericalMethod'
+import {derivative, MathNode, simplify} from 'mathjs'
+
+class TaylorSeries extends NumericalMethod {
+
+    iterations: string[]
+
+    private constructor(iterations: string[]) {
+        super()
+        this.iterations = iterations
+    }
+
+    public static create(functionFx: string, iterations: number): TaylorSeries | null {
+
+        const parsedFunctionFx = this.getParsedFunction(functionFx)
+        if (!parsedFunctionFx) {
+            console.log("TaylorSeries: Error parsing function")
+            return null
+        }
+        if (iterations < 0) {
+            console.log("TaylorSeries: Iterations number must be positive")
+            return null
+        }
+        const taylorIterations = this.method(parsedFunctionFx, iterations)
+        if (!taylorIterations) {
+            console.log("TaylorSeries: Error computing method")
+            return null
+        }
+        return new TaylorSeries(taylorIterations)
+    }
+
+    public static method(functionFx: MathNode, iterations: number, currentIteration: number = 0): string[] {
+
+        const derivativeFunctionFx = currentIteration === 0 ? functionFx : derivative(functionFx, "x")
+        const derivativeEvaluated = this.evaluate(derivativeFunctionFx, 0)
+
+        const iteration = simplify(
+            `(${derivativeEvaluated}/${this.factorial(currentIteration)})*x^${currentIteration}`)
+
+        if (currentIteration === iterations) {
+            return [iteration.toTex()]
+        }
+
+        return [iteration.toTex()].concat(
+            this.method(derivativeFunctionFx, iterations, currentIteration + 1))
+    }
+}
+
+export default TaylorSeries
---
+++ src/pages/TaylorSeriesPage.vue
@@ -0,0 +1,108 @@
+<template>
+  <AppContentLayout>
+    <div class="columns">
+      <div class="column">
+        <b-field label="Función" label-position="inside">
+          <b-input v-model="inputFunction" @input="updatePlot"/>
+        </b-field>
+        <b>Número de iteraciones</b>
+        <b-field grouped>
+          <AppNumberInput label="Iteraciones" v-model="iterations" :min="1" @input="updatePlot"/>
+        </b-field>
+
+        <katex-element :expression="parsedInputFunction"/>
+
+        <div v-if="taylorSeries">
+
+          <katex-element :expression="approximationSum"/>
+
+          <b-field :label="'Iteración ' + (this.selectedIteration) + '/' + (this.taylorSeries.iterations.length)">
+            <b-slider v-model="selectedIteration" :min="1" :max="this.taylorSeries.iterations.length" ticks/>
+          </b-field>
+
+          <b-button @click="updatePlot">Click me</b-button>
+          {{taylorSeries}}
+        </div>
+      </div>
+
+      <div class="column">
+        <vue-iframe :src="plotUrl" @load="onLoadIframe"/>
+      </div>
+    </div>
+  </AppContentLayout>
+</template>
+
+<script lang="ts">
+import TaylorSeries from '@/methods/TaylorSeries'
+import {Component, Vue, Watch} from 'vue-property-decorator'
+
+import AppContentLayout from '@/components/layout/AppContentLayout.vue'
+import AppNumberInput from '@/components/AppNumberInput.vue'
+
+@Component({
+  components: {AppContentLayout, AppNumberInput}
+})
+
+export default class TaylorSeriesPage extends Vue {
+
+  inputFunction: string = "e^x"
+  iterations: number = 3
+
+  selectedIteration: number = 1
+
+  plot: Window | null = null
+  plotUrl = `${process.env.BASE_URL}/plot/taylor-series.html`
+
+  get taylorSeries(): TaylorSeries | null {
+    return TaylorSeries.create("e^x", this.iterations)
+  }
+
+  get approximationSum(): string | null {
+    if (!this.taylorSeries) {
+      return null
+    }
+    const filterIterations = this.taylorSeries.iterations.slice(0, this.selectedIteration)
+    const sum = filterIterations.reduce((approximationSum, iteration) => `${approximationSum}+${iteration}`, "")
+    return `f(x)=${sum}`
+  }
+
+  get parsedInputFunction(): string{
+    return TaylorSeries.getFxEquation(this.inputFunction)
+  }
+
+  updatePlot() {
+    if (!this.plot) {
+      return
+    }
+
+    let message: any = {}
+    if (this.taylorSeries) {
+      message["functionFx"] = this.parsedInputFunction
+      message["approximationFx"] = this.approximationSum
+    }
+    this.plot.postMessage(message, "*")
+  }
+
+  onLoadIframe(frame: HTMLFrameElement) {
+    const window = frame.contentWindow
+
+    if (window)
+      this.plot = window
+  }
+
+  @Watch("taylorSeries")
+  onTaylorSeries() {
+    this.updatePlot()
+  }
+
+  @Watch("selectedIteration")
+  onSelectedIteration() {
+    this.updatePlot()
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
