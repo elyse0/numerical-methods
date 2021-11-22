1a2b8f48c745c20b865f16c2fff8779d49faadd0
Elyse <kevincristian@outlook.com>
2021-11-21 23:42:10 -0600

    AppPlot: Add AppPlot component

---
+++ src/components/AppPlot.vue
@@ -0,0 +1,30 @@
+<template>
+  <vue-iframe :src="plotUrl" @load="onLoadIframe"/>
+</template>
+
+<script lang="ts">
+import {Component, Prop, VModel, Vue} from 'vue-property-decorator'
+
+@Component
+export default class AppPlot extends Vue {
+
+  @VModel({default: null}) plot!: Window | null
+  @Prop({required: true}) name!: string
+
+  plotUrl = `${process.env.BASE_URL}/plot/${this.name}.html`
+
+  onLoadIframe(frame: HTMLFrameElement) {
+    const window = frame.contentWindow
+
+    console.log("Loaded")
+
+    if (window) {
+      this.plot = window
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
