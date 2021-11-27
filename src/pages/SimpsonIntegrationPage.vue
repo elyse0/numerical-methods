79acef32d530c30194260294f2bd7550bbba9d5c
Elyse <kevincristian@outlook.com>
2021-11-27 10:27:10 -0600

    SimpsonIntegration: Add input boxes to compute method

--- src/pages/SimpsonIntegrationPage.vue
+++ src/pages/SimpsonIntegrationPage.vue
@@ -6,11 +6,25 @@
     </template>
 
     <template #content>
-      <div v-if="simpsonIntegration">
+
+      <b-field label="Integral" label-position="inside">
+        <b-input v-model="inputFunction"/>
+      </b-field>
+
+      <b>Intervalo de integracion</b>
+      <b-field grouped>
+        <AppNumberInput label="a" v-model="integrationInterval.x0" @input="updatePlot"/>
+        <AppNumberInput label="b" v-model="integrationInterval.x1" @input="updatePlot"/>
+        <AppNumberInput label="Número de pasos" v-model="steps" :min="6" :max="100" @input="updatePlot"/>
+
+      </b-field>
+
+      <div>
         Type: {{simpsonIntegration.type}}
-        <katex-element :expression="simpsonIntegration.integralFunction"/>
-        {{simpsonIntegration.integral}}
-        <b-button @click="updatePlot">Click me</b-button>
+      </div>
+
+      <div v-if="integrationLatex" class="has-text-centered">
+        <katex-element :expression="integrationLatex"/>
       </div>
     </template>
 
@@ -27,20 +41,38 @@ import {Component, Vue} from 'vue-property-decorator'
 import AppContentAndPlot from '@/components/layout/AppContentAndPlot.vue'
 import AppHero from '@/components/AppHero.vue'
 import AppPlot from '@/components/AppPlot.vue'
+import AppNumberInput from '@/components/AppNumberInput.vue'
 
 import SimpsonIntegration from '@/methods/SimpsonIntegration'
+import {IntegrationInterval} from '@/methods/NumericalMethod'
 
 @Component({
-  components: {AppContentAndPlot, AppHero, AppPlot}
+  components: {AppContentAndPlot, AppHero, AppPlot, AppNumberInput}
 })
 
 export default class SimpsonIntegrationPage extends Vue {
+
+  inputFunction: string = "1/(1+x)"
+  integrationInterval: Partial<IntegrationInterval> = {x0: 0, x1: 1}
+  steps: number = 6
 
   plot: Window | null = null
 
   get simpsonIntegration(): SimpsonIntegration | null {
 
-    return SimpsonIntegration.create("1/(1+x)", {x0: 0, x1: 1}, 6)
+    return SimpsonIntegration.create(this.inputFunction, this.integrationInterval, this.steps)
+  }
+
+  get integrationLatex(): string | null {
+
+    if (!this.simpsonIntegration) {
+      return null
+    }
+
+    const integralFunction = this.simpsonIntegration.integralFunction
+    const integralValue = this.simpsonIntegration.integral.toFixed(4)
+
+    return `${integralFunction}=${integralValue}`
   }
 
   updatePlot() {
