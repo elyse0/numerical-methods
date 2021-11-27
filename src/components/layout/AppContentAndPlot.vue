5704d806f0558a198ff394bf60c692c3f1bd1feb
Elyse <kevincristian@outlook.com>
2021-11-26 19:53:17 -0600

    AppContentAndPlot: Add layout slots for pages with content and plot

---
+++ src/components/layout/AppContentAndPlot.vue
@@ -0,0 +1,32 @@
+<template>
+  <AppContentLayout>
+    <slot name="header"/>
+
+    <div class="columns">
+      <div class="column">
+        <slot name="content"/>
+      </div>
+      <div class="column">
+        <slot name="plot"/>
+      </div>
+    </div>
+
+  </AppContentLayout>
+</template>
+
+<script lang="ts">
+import AppContentLayout from '@/components/layout/AppContentLayout.vue'
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component({
+  components: {AppContentLayout}
+})
+
+export default class AppContentAndPlot extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
