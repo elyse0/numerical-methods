484d328b8bf28a586fda138c82520bbf9c33549b
Elyse <kevincristian@outlook.com>
2021-11-28 14:00:11 -0600

    AppMatrix: Use AppNumberInput

--- src/components/AppMatrix.vue
+++ src/components/AppMatrix.vue
@@ -3,36 +3,28 @@
     <div class="columns">
       <div class="column has-text-centered" v-for="column in matrix[0].length" :key="column">
         <div class="row" v-for="row in matrix.length" :key="row" style="padding-bottom: 10px">
-          <b-numberinput :controls="false" v-model="matrix[row - 1][column - 1]">
-          </b-numberinput>
+          <AppNumberInput v-model="matrix[row - 1][column - 1]" label=""/>
         </div>
       </div>
     </div>
-    Valid: {{valid}}
   </div>
 </template>
 
 <script lang="ts">
 import {Component, VModel, Vue, Watch} from 'vue-property-decorator'
 
-@Component
+import AppNumberInput from '@/components/AppNumberInput.vue'
+
+@Component({
+  components: {AppNumberInput}
+})
+
 export default class AppMatrix extends Vue {
 
   @VModel({
     required: true,
     type: Array as () => (Array<Array<number | null>> | null)
   }) matrix!: Array<Array<number | null>>
-
-  get valid(): boolean {
-    if (!this.matrix) {
-      return false
-    }
-
-    const flat = this.matrix.flat()
-    const valid = flat.some((value: number | null) => value === null)
-
-    return !valid
-  }
 
   @Watch("matrix")
   onMatrix() {
